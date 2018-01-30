23f12507c6b8bebe65fa5f02a5369c8893d1f8bc
Unknown <ratchanok1343@gmail.com>
2018-01-30 11:49:26 +0700

    1-2 เสร็จแล้ว

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <fstream>
+ using namespace std;
 int main() {
-        /* code */
+        ofstream myfile ("exaple.txt");
+        if (myfile.is_open())
+        {
+            myfile << "This ids a line.\n";
+            myfile << "This ids another line.\n";
+            myfile.close();
+        }
+        else cout <<"Unble to open file";
         return 0;
 }
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+#include <fstream>
+ using namespace std;
 int main() {
-        /* code */
+    string line;
+    ifstream myfile ("example.txt");
+    if(myfile.is_open())
+    {
+
+        while (getline (myfile.line))
+        {
+            cout <<line<<'\n';
+        }
+        myfile.close();
+        }
+    else cout << "unble to open file";
+
         return 0;
 }
